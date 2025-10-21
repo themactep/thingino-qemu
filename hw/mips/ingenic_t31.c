762372d563ec01851999446759bf7f3d9193fd24
Paul Philippov <paul@themactep.com>
2025-10-21 03:27:16 -0400

    mips: thingino-t31: switch console to UART0 @ 0x10030000, 115200; keep INT4 wiring; pflash 16MiB

--- hw/mips/ingenic_t31.c
+++ hw/mips/ingenic_t31.c
@@ -14,7 +14,7 @@
 #include "target/mips/cpu.h"
 #include "hw/qdev-clock.h"
 
-#define T31_UART1_PHYS 0x10031000u /* vendor UART0_BASE 0xB0030000 (KSEG1) */
+#define T31_UART0_PHYS 0x10030000u /* vendor UART0_BASE 0xB0030000 (KSEG1) */
 #define T31_RAM_BASE   0x00000000u
 #define T31_RESET_ADDR 0x1FC00000u /* maps to KSEG1 0xBFC00000 */
 #define T31_BIOS_SIZE  (16 * MiB)
@@ -38,12 +38,12 @@ static void t31_init(MachineState *machine)
     /* Map main RAM at 0x0 */
     memory_region_add_subregion(sysmem, T31_RAM_BASE, machine->ram);
 
-    /* UART1 at Ingenic T31 address; use regshift=2 (32-bit spaced regs). */
+    /* UART0 at Ingenic T31 address; use regshift=2 (32-bit spaced regs). */
     MIPSCPU *pcpu = MIPS_CPU(first_cpu);
     CPUMIPSState *env = &pcpu->env;
     qemu_irq uirq = env->irq[4];
-    serial_mm_init(sysmem, T31_UART1_PHYS, /*regshift*/2, uirq,
-                   24000000, serial_hd(0), DEVICE_NATIVE_ENDIAN);
+    serial_mm_init(sysmem, T31_UART0_PHYS, /*regshift*/2, uirq,
+                   115200, serial_hd(0), DEVICE_NATIVE_ENDIAN);
 
     /* Optional: register a pflash (if provided via -drive if=pflash) at reset addr */
     {
@@ -65,7 +65,7 @@ static void t31_init(MachineState *machine)
 static void t31_machine_class_init(ObjectClass *oc, const void *data)
 {
     MachineClass *mc = MACHINE_CLASS(oc);
-    mc->desc = "Thingino Ingenic T31 (UART@0x10031000, pflash@0x1FC00000)";
+    mc->desc = "Thingino Ingenic T31 (UART@0x10030000, pflash@0x1FC00000)";
     mc->init = t31_init;
     mc->default_cpu_type = MIPS_CPU_TYPE_NAME("24Kf");
     mc->default_ram_id = "t31.ram";
